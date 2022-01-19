59206a24ac3ec18f8326ce5f414b8e892e015817
XXKEVSXX <98014109+XXKEVSXX@users.noreply.github.com>
2022-01-19 16:06:36 +0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int DorM= 0, hw, dw, late, dayabsent;
+double totalGross, totalnetpay, salary, rate, Philhealth, sss, tax, Pagbig,
+char opt;
+Cout<<"-- Daily rate -- payroll Program --"<endl;
+Cout<<"[1] daily rate "<<end1;
+Cout<<"[2] Exit " << end;
+Cout<<" Select : ";
+Cin>> DorM;
+If (DorM==1){
+Cout<<"-- Payroll system -- Daily Rate -- "<<end1;
+Cout<<" Enter Rate : ";
+Cin>> Rate;
+Cout<< " Enter House of works : ";
+Cin>> hw ;
+Cout<< "Enter Days of work : ";
+Cin>> dw ;
+Cout<< ""end1;
+Total Gross = rate * hw * dw;
+Cout << "total Gross: " << total Gross <<end1;
+}
+return 0;
---
+++ hugo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int DorM= 0, hw, dw, late, dayabsent;
+double totalGross, totalnetpay, salary, rate, Philhealth, sss, tax, Pagbig,
+
+double opt;
+cout<<"-- Daily rate -- payroll Program --"<<endl;
+cout<<"[1] daily rate "<<endl;
+cout<<"[2] Exit " << endl;
+cout<<" Select : ";
+cin>> DorM;
+
+if (DorM==10) {
+cout<<"-- Payroll system -- Daily Rate -- "<<endl;
+cout<<" Enter Rate : ";
+cin>> rate;
+cout<< " Enter House of works : ";
+cin>> hw ;
+cout<< "Enter Days of work : ";
+cin>> dw ;
+cout<< ""<<endl;
+Total Gross = rate * hw * dw;
+cout << "total Gross: " <<"total Gross "<<endl;
+}
+
